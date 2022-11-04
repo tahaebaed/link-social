2116ec796ddc08b5499de225ed69fe732358a352
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-11-04 23:44:35 +0200

    sprint 1 - Button component - create button JSDoc

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import '../assets/sass/components/buttons.scss';
 
+/**
+ * @usage
+- use `className` to add any additional className
+- use `outline` to create outline buttons
+- use `sm` to create small buttons
+- use `lg` to create bigger buttons
+- use `disabled` to disabled buttons actions
+
+ * @example
+
+<Button> Button </Button/>
+<Button lg> Button </Button/>
+<Button sm> Button </Button/>
+<Button disabled> Button </Button/>
+
+// Outline
+<Button outline> Button </Button/>
+<Button outline lg> Button </Button/>
+<Button outline sm> Button </Button/>
+<Button outline disabled> Button </Button/>
+
+ * @param {Object} props 
+ * @returns
+ */
 function Button(props) {
 	return <button>{props.children}</button>;
 }

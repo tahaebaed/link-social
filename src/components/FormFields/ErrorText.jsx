79ee2a110757ce3c2e4b99e06816b031058fba8b
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-08 14:23:40 +0200

    sprint 2 - edit text font size

--- src/components/FormFields/ErrorText.jsx
+++ src/components/FormFields/ErrorText.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const ErrorText = (props) => {
-	return <div className='px-3 py-2 text-rose-600 '>{props.children}</div>;
+	return (
+		<div className='px-3 py-2 text-xs text-rose-600 '>{props.children}</div>
+	);
 };
 
 export default ErrorText;

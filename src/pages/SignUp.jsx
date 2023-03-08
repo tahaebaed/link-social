63ec91f33f36d49a8616036a17f7b0ba4ae42e4b
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-08 14:34:40 +0200

    sprint 2 - Sign up component - edit style

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BsPerson } from 'react-icons/bs';
 import { HiOutlineMail } from 'react-icons/hi';
+import { AiOutlineLock, AiOutlineUnlock } from 'react-icons/ai';
 import { useDispatch } from 'react-redux';
 import { Form, Formik } from 'formik';
 import * as yup from 'yup';
@@ -27,15 +28,15 @@ const SignUp = () => {
 	const validationSchema = yup.object({
 		user_name: yup
 			.string()
-			.required('please your username is required')
+			.required('Your username is required')
 			.matches(/^[-\w.$@*!]{1,30}$/, "username mustn't contains any spaces"),
 		email: yup
 			.string()
 			.email('please enter a valid email')
-			.required('please your email is required'),
+			.required('Your email is required'),
 		password: yup
 			.string()
-			.required('please your password is required')
+			.required('Your password is required')
 			.min(8, 'Your password is too short.')
 			.matches(
 				/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
@@ -43,10 +44,10 @@ const SignUp = () => {
 			),
 		password_confirmation: yup
 			.string()
-			.required('please reenter you password as it required field')
+			.required('Password confirmation is required')
 			.oneOf([yup.ref('password'), null], 'Passwords must match'),
-		first_name: yup.string().required('please your first name is required'),
-		last_name: yup.string().required('please your last name is required'),
+		first_name: yup.string().required('Your first name is required'),
+		last_name: yup.string().required('Your last name is required'),
 		phone: yup.string(),
 		age: yup.string(),
 		gender: yup.string(),
@@ -63,120 +64,130 @@ const SignUp = () => {
 			onSubmit={onSubmit}
 		>
 			{(formik) => (
-				<Form className='h-[30rem] w-full text-sm'>
-					<div className='w-full'>
-						<div className='flex'>
-							<div className='w-3/6'>
-								<h2>User Name</h2>
-								<FormikControl
-									icon={<BsPerson />}
-									name='user_name'
-									id='user_name'
-									type='text'
-									label='user name'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
+				<div>
+					<h2 className='text-3xl mb-10 text-red-400'>Sign Up</h2>
+					<Form className='w-full text-sm'>
+						<div className='w-full'>
+							<div className='flex'>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>User Name</h2>
+									<FormikControl
+										icon={<BsPerson />}
+										name='user_name'
+										id='user_name'
+										type='text'
+										label='user name'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>Email</h2>
+									<FormikControl
+										name='email'
+										id='email'
+										icon={<HiOutlineMail />}
+										type='email'
+										label='email'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
 							</div>
-							<div className='w-3/6'>
-								<h2>Email</h2>
-								<FormikControl
-									name='email'
-									id='email'
-									icon={<HiOutlineMail />}
-									type='email'
-									label='email'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
+							<div className='flex'>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>Password</h2>
+									<FormikControl
+										name='password'
+										id='password'
+										icon={<AiOutlineLock />}
+										control='password'
+										label='password'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>Confirm Password</h2>
+									<FormikControl
+										name='password_confirmation'
+										id='password_confirmation'
+										icon={<AiOutlineUnlock />}
+										control='password'
+										label='confirm password'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
 							</div>
+
+							<div className='flex w-full'>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>First Name</h2>
+									<FormikControl
+										name='first_name'
+										id='first_name'
+										icon={<BsPerson />}
+										label='first name'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>Last Name</h2>
+
+									<FormikControl
+										name='last_name'
+										id='last_name'
+										icon={<BsPerson />}
+										label='last name'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
+							</div>
+							<h2 className='mb-4 ml-2'>Phone</h2>
+
+							<FormikControl
+								name='phone'
+								id='phone'
+								label='phone (optional)'
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+							/>
+							<label className='flex'>
+								<div className='w-3/6'>
+									<h2 className='mb-4 ml-2'>Age </h2>
+
+									<FormikControl
+										name='age'
+										id='age'
+										label='age (optional)'
+										onChange={formik.handleChange}
+										onBlur={formik.handleBlur}
+									/>
+								</div>
+								<div className='w-3/6 flex'>
+									<h2 className='mb-4 ml-2'>Gender</h2>
+									<FormikControl
+										className='flex'
+										control='radio'
+										label='gender (optional)'
+										options={[
+											{ value: 'male', key: 'male' },
+											{ value: 'female', key: 'female' },
+										]}
+										name='gender'
+										id='gender'
+									/>
+								</div>
+							</label>
 						</div>
-						<div className='flex'>
-							<div className='w-3/6'>
-								<h2>Password</h2>
-								<FormikControl
-									name='password'
-									id='password'
-									control='password'
-									label='password'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
-							</div>
-							<div className='w-3/6'>
-								<h2>Confirm Password</h2>
-								<FormikControl
-									name='password_confirmation'
-									id='password_confirmation'
-									control='password'
-									label='confirm password'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
-							</div>
-						</div>
-
-						<div className='flex w-full'>
-							<div className='w-3/6'>
-								<h2>First Name</h2>
-								<FormikControl
-									name='first_name'
-									id='first_name'
-									label='first name'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
-							</div>
-							<div className='w-3/6'>
-								<h2>Last Name</h2>
-
-								<FormikControl
-									name='last_name'
-									id='last_name'
-									label='last name'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
-							</div>
-						</div>
-						<h2>Phone</h2>
-
-						<FormikControl
-							name='phone'
-							id='phone'
-							label='phone (optional)'
-							onChange={formik.handleChange}
-							onBlur={formik.handleBlur}
-						/>
-						<label className='flex'>
-							<div className='w-2/6'>
-								<h2>age </h2>
-
-								<FormikControl
-									name='age'
-									id='age'
-									label='age (optional)'
-									onChange={formik.handleChange}
-									onBlur={formik.handleBlur}
-								/>
-							</div>
-							<div className='w-4/5 flex'>
-								<FormikControl
-									className='flex'
-									control='radio'
-									label='gender (optional)'
-									options={[
-										{ value: 'male', key: 'male' },
-										{ value: 'female', key: 'female' },
-									]}
-									name='gender'
-									id='gender'
-								/>
-							</div>
-						</label>
-					</div>
-					<button type='submit'>submit</button>
-				</Form>
+						<Button className='bg-orange-400 border-orange-400 hover:text-orange-400'>
+							Submit
+						</Button>
+					</Form>
+				</div>
 			)}
 		</Formik>
 	);

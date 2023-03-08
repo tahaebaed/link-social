f47c1474788456c2eb89717090fdde56a458dae0
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-08 14:26:40 +0200

    adding margin to inputs

--- src/components/FormFields/Input.jsx
+++ src/components/FormFields/Input.jsx
@@ -5,7 +5,7 @@ import ErrorText from './ErrorText';
 const Input = (props) => {
 	const { name, label, icon, type } = props;
 	return (
-		<div className='relative mb-5' style={{ height: '60px' }}>
+		<div className='relative mb-5 mx-2' style={{ height: '60px' }}>
 			<span
 				className='icon absolute p-1 rounded-full w-fit left-2 opacity-40'
 				style={{ top: '17%' }}
@@ -13,7 +13,7 @@ const Input = (props) => {
 				{icon}
 			</span>
 			<Field
-				className='rounded-full border py-2 pl-9 w-full '
+				className='rounded-full outline-amber-100 border py-2 pl-9 w-full '
 				id={name}
 				name={name}
 				placeholder={label}

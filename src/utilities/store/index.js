4cf439adeac658ef6cfec25addf3b4aa054e122d
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-09 17:14:56 +0200

    sprint 2 - store - add weather reducer

--- src/utilities/store/index.js
+++ src/utilities/store/index.js
@@ -1,15 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
-
+import weatherReducer from './weather_reducer/weatherSlice'
 import userReducer from './user_reducer/userSlice'
 
-const store = configureStore({
-  reducer: { userReducer },
-  devTools: process.env !== 'production',
+export const store = configureStore({
+	reducer: { userReducer, weatherReducer },
+	devTools: process.env !== 'production',
 })
 
 const ReduxProvider = ({ children }) => (
-  <Provider store={store}>{children}</Provider>
+	<Provider store={store}>{children}</Provider>
 )
 
 export default ReduxProvider

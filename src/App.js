d78b5bb15138963011d0b7c85800b056c6fa0423
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-09 16:54:25 +0200

    sprint 2 - app component - add ip call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import './App.scss';
 import Navbar from './layout/Navbar.jsx';
+import { ipInstance } from './utilities/weatherCard/ipInstance';
+
 
 function App() {
+	useEffect(() => {
+		ipInstance.get("")
+	}, [])
 	return (
 		<div className='App'>
 			<Navbar />

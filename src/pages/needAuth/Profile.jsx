bd5a32cd96478e66bf0236e54aedaf6053f5ccf7
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-03-20 21:10:33 +0200

    sprint-4 - mohammed - refactor profile page

--- src/pages/needAuth/Profile.jsx
+++ src/pages/needAuth/Profile.jsx
@@ -1,67 +1,109 @@
-import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import ProfileNavbar from '../../layout/Profile/ProfileNavbar';
-import ProfileSidebar from '../../layout/Profile/ProfileSidebar';
-import './../../assets/scss/components/profile.scss';
+import { HiOutlineChatBubbleLeftRight } from 'react-icons/hi2';
+import { IoPersonAdd } from 'react-icons/io5';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useParams, useSearchParams } from 'react-router-dom';
+import Button from '../../components/Button';
+import usePageTitle from '../../hooks/usePageTitle';
+import { Navs, Taps } from '../../layout/Profile/TapsAndNavs';
+import { getUser, getUserPosts } from '../../utilities/store/profile.slice';
+import { profileSelector } from '../../utilities/store';
 
 const Profile = () => {
-	const [response, setResponse] = useState(null);
+	usePageTitle('Profile');
+	const dispatch = useDispatch();
+	const params = useParams();
+	const { isLoading, error, profile } = useSelector(profileSelector.about);
+	const [searchParams, setSearchParams] = useSearchParams();
+	const [activeTap, setActiveTap] = useState('about');
+
 	useEffect(() => {
-		getData();
+		const sp = searchParams.get('activeTap');
+		if (sp) {
+			setActiveTap(sp);
+		}
+	}, [searchParams]);
+
+	useEffect(() => {
+		dispatch(getUser(params?.profileId));
+		dispatch(getUserPosts(params?.profileId));
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
-	// Fetching data
-	const getData = async () => {
-		await axios
-			.get('https://link-social.up.railway.app/api/v1/profiles/10')
-			.then((res) => {
-				setResponse(res.data.data);
-			})
-			.catch((error) => {
-				// handle error
-				console.log(error);
-			});
+
+	const onActiveTapChange = (tapId) => {
+		setActiveTap(tapId);
+		setSearchParams((prev) => {
+			prev.set('activeTap', tapId);
+			return prev;
+		});
+	};
+
+	const ProfileWrapper = () => {
+		if (isLoading) {
+			return <>Loading...</>;
+		} else if (error) {
+			return <pre>{JSON.stringify(error)}</pre>;
+		} else if (profile) {
+			return (
+				<>
+					<div className='p-3 shadow-[0_8px_30px_rgba(0,_0,_0,_0.05)]'>
+						<img
+							src={profile.cover}
+							alt={profile.user.first_name + '-cover'}
+							className='mb-3 rounded-xl h-[250px] w-full object-cover max-w-full'
+						/>
+						<div className='relative md:flex items-center my-4'>
+							<img
+								src={profile.avatar}
+								alt={profile.user.first_name + '-avatar'}
+								className='object-cover border-white w-28 h-28 absolute rounded-full border-4 top-[-40px] left-[30px] z-10'
+							/>
+							<div className='pl-40'>
+								<h3 className='text-2xl font-bold'>
+									{profile.user.first_name} {profile.user.last_name}
+								</h3>
+								<div className='text-gray-400 text-sm'>
+									{profile.user.email}
+								</div>
+							</div>
+							<div className='ml-auto mt-4 md:mt-0'>
+								<div className='flex justify-end'>
+									<Button
+										as={Link}
+										to={`/add-friend/${profile.id}`}
+										className='font-bold mx-2 flex items-center'
+									>
+										<IoPersonAdd className='inline-block mr-2' />
+										<span>Add Friend</span>
+									</Button>
+									<Button as={Link} to={`/message/${profile.id}`} outline lg>
+										<HiOutlineChatBubbleLeftRight />
+									</Button>
+								</div>
+							</div>
+						</div>
+						<div>
+							<Taps
+								activeTap={activeTap}
+								onActiveTapChange={onActiveTapChange}
+							/>
+						</div>
+					</div>
+					<Navs activeTap={activeTap} user={profile} />
+				</>
+			);
+		} else {
+			return <>no user found</>;
+		}
 	};
 
 	return (
-		<div className='bg-slate-100'>
-			{response && (
-				<div className='container'>
-					<div className='profile__header relative'>
-						<img
-							src={
-								response.profile.cover
-									? `https://link-social.up.railway.app/${response.profile.cover}`
-									: 'https://via.placeholder.com/150'
-							}
-							alt={response.profile.user.user_name}
-							className='bg__image  rounded-t-xl h-[40vh] w-full  object-fill'
-						/>
-						<img
-							src={
-								response.profile.avatar
-									? `https://link-social.up.railway.app/${response.profile.avatar}`
-									: 'https://via.placeholder.com/150'
-							}
-							alt={response.profile.user.user_name}
-							className='profile-img object-contain absolute rounded-full top-[55%] border-4 left-2/4 translate-x-[-50%] z-10'
-						/>
-						<ProfileNavbar
-							username={response.profile.user.user_name}
-							useremail={response.profile.user.email}
-						/>
-					</div>
-					<div className='profile__body flex flex-wrap'>
-						<div className='flex-auto md:w-1/3 '>
-							<ProfileSidebar data={response.profile} />
-						</div>
-						<div className='flex-auto w-full md:w-2/3'>
-							<Outlet />
-						</div>
-					</div>
-				</div>
-			)}
-		</div>
+		<section className='relative md:grid grid-cols-4 min-h-screen items-start justify-center overflow-y-hidden border'>
+			<nav className=' z-[1035] h-screen col-span-1 bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] '></nav>
+			<div className='p-5 col-span-3 '>
+				<ProfileWrapper />
+			</div>
+		</section>
 	);
 };
 

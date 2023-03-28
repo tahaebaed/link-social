4e41f3bc7f1831d18ec77a6267b4002e975578f1
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-28 22:33:36 +0200

    add reusable profile img component

---
+++ src/components/ProfileImg.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import profilePic from '../assets/images/imgs/profilePic.png';
+function ProfileImg({ img = profilePic, border, online, ...props }) {
+	const [className, setClassName] = useState('');
+	const [onlineState, setOnlineState] = useState('');
+	useEffect(() => {
+		let initClass =
+			`rounded-full w-full h-full object-cover p-1 w-[45px] h-[45px] ` +
+			props.className;
+		let initOnline = `profile_img relative`;
+
+		initClass += border ? ' border border-emerald-400' : '';
+		initOnline += online
+			? ` before:content-[''] before:absolute before:w-[10px] before:h-[10px] before:bg-aurora before:rounded-full before:left-1 `
+			: '';
+		setClassName(initClass);
+		setOnlineState(initOnline);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+	return (
+		<div className={onlineState}>
+			<img {...props} className={className} src={img} alt='profile img' />
+		</div>
+	);
+}
+ProfileImg.defaultProps = {
+	border: false,
+	className: '',
+};
+
+export default ProfileImg;

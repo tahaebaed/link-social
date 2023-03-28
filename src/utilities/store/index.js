344ac5adc96d265bcf6211833ffc022dd7a73d60
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-03-28 22:49:07 +0200

    sprint 4 -store - add get posts api

--- src/utilities/store/index.js
+++ src/utilities/store/index.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import weatherReducer from './weather_reducer/weatherSlice'
+import postsReducer from './posts_reducer/postsSlice'
 import userReducer from './user_reducer/userSlice'
 
 export const store = configureStore({
-	reducer: { userReducer, weatherReducer },
+	reducer: { userReducer, weatherReducer, postsReducer },
 	devTools: process.env !== 'production',
 })
 

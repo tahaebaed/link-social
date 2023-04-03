a2bc0a50cedf948b7ae56701289d910efd8487fa
hana16-6 <64643374+hana16-6@users.noreply.github.com>
2023-04-03 17:22:28 +0200

    sprint 5 - edit style

--- src/layout/FriendsList.jsx
+++ src/layout/FriendsList.jsx
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Dropdown from '../components/Dropdown';
 import ProfileImg from '../components/ProfileImg';
 import { friends } from '../utilities/dummydata/users';
+import '../assets/scss/layout/friendList.scss';
 function FriendsList() {
-	const [scrollTop, setScrollTop] = useState(0);
-	// useEffect(() => {
-	// 	const handleScroll = (event) => {
-	// 		console.log(window.scrollY, 'kkk');
-	// 		setScrollTop(window.scrollY);
-	// 	};
-
-	// 	window.addEventListener('scroll', handleScroll);
-
-	// 	return () => {
-	// 		window.removeEventListener('scroll', handleScroll);
-	// 	};
-	// }, []);
 	return (
-		<div className={`friends_list fixed top-0 h-[100vh] overflow-hidden`}>
-			<div className='h-[100vh] overflow-hidden bottom-0 my-5'>
+		<div className={`friends_list fixed right-0 bottom-0 h-[92vh]`}>
+			<div className='friends_list_box h-[78.5vh] overflow-auto bottom-0 my-5'>
 				{friends.map((friend, i) => (
 					<div className='sigle_user m-4 grid grid-cols-6' key={i}>
 						<div className='user_img col-span-1'>
@@ -56,8 +43,7 @@ function FriendsList() {
 					</div>
 				))}
 			</div>
-
-			<div className='show_btn absolute bottom-0 w-full bg-aurora p-4 text-center mx-3'>
+			<div className='show_btn absolute bottom-0 w-full bg-aurora p-4 text-center'>
 				<Link to='/auth/sign-in' className='text-white'>
 					Show All Friends
 				</Link>

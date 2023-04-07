3a7193004a702a4f95182956534823c8561553ff
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-07 16:53:42 +0200

    sprint-5 - mohammed - add setting set user

--- src/utilities/store/setting.slice.js
+++ src/utilities/store/setting.slice.js
@@ -1,8 +1,21 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { userInterceptor } from '../../apps/axiosInstance';
-import { toast } from 'react-toastify';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import Cookies from 'js-cookie';
+import { toast } from 'react-toastify';
+import { userInterceptor } from '../../apps/axiosInstance';
 import { loginUser } from './user_reducer/userSlice';
+
+const getUser = createAsyncThunk('setting/get/user', async () => {
+	try {
+		const profileId = JSON.parse(Cookies.get('user') || '{}')?.id;
+		const response = await userInterceptor({
+			method: 'get',
+			url: `/users/${profileId}`,
+		});
+		return response.data;
+	} catch (err) {
+		return { error: err.response, status: false };
+	}
+});
 
 const putUpdateUser = createAsyncThunk(
 	'setting/put/user',
@@ -21,11 +34,32 @@ const putUpdateUser = createAsyncThunk(
 	}
 );
 
+const putUpdateProfile = createAsyncThunk(
+	'setting/put/profile',
+	async (profile, { dispatch }) => {
+		try {
+			const response = await userInterceptor({
+				method: 'put',
+				url: `/profiles/`,
+				data: profile,
+				headers: {
+					'Content-Type': 'multipart/form-data',
+				},
+			});
+			dispatch(loginUser(response.data.data.user));
+			return response.data;
+		} catch (err) {
+			return { error: err.response, status: false };
+		}
+	}
+);
+
 const setting = createSlice({
 	name: 'setting',
 	initialState: {
 		isLoading: false,
 		error: null,
+		user: null,
 	},
 	reducers: {},
 	extraReducers: {
@@ -38,6 +72,7 @@ const setting = createSlice({
 			if (action.payload.status) {
 				toast.success(action.payload.message);
 				Cookies.set('user', JSON.stringify(action.payload.data.user));
+				state.user = action.payload.data.user;
 			} else {
 				state.error = action.payload.error.data;
 				toast.error(action.payload.error.data.message);
@@ -47,8 +82,44 @@ const setting = createSlice({
 			state.error = action.payload.error.data;
 			toast.error(action.payload.error.data.message);
 		},
+		[putUpdateProfile.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[putUpdateProfile.fulfilled]: (state, action) => {
+			state.isLoading = false;
+
+			if (action.payload.status) {
+				toast.success(action.payload.message);
+				Cookies.set('user', JSON.stringify(action.payload.data.user));
+				state.user = action.payload.data.user;
+			} else {
+				state.error = action.payload.error.data;
+				toast.error(action.payload.error.data.message);
+			}
+		},
+		[putUpdateProfile.rejected]: (state, action) => {
+			state.error = action.payload.error.data;
+			toast.error(action.payload.error.data.message);
+		},
+		[getUser.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[getUser.fulfilled]: (state, action) => {
+			state.isLoading = false;
+
+			if (action.payload.status) {
+				state.user = action.payload.data.user;
+			} else {
+				state.error = action.payload.error.data;
+				toast.error(action.payload.error.data.message);
+			}
+		},
+		[getUser.rejected]: (state, action) => {
+			state.error = action.payload.error.data;
+			toast.error(action.payload.error.data.message);
+		},
 	},
 });
 
 export default setting.reducer;
-export { putUpdateUser };
+export { putUpdateUser, putUpdateProfile, getUser };

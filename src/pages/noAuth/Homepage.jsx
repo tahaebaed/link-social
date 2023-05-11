3c6de98992f6bea8446d8a60d9d394db18292d07
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-11 11:43:03 +0300

    sprint-5 - mohammed - refactor homepage

--- src/pages/noAuth/Homepage.jsx
+++ src/pages/noAuth/Homepage.jsx
@@ -1,33 +1,25 @@
 import React from 'react';
 import CreatePost from '../../components/CreatePost';
 import usePageTitle from '../../hooks/usePageTitle';
-import CalendarCard from '../../layout/CalendarCard';
 import FriendsList from '../../layout/FriendsList';
 import HomeFeeds from '../../layout/HomeFeeds';
-import WeatherCard from '../../layout/WeatherCard';
 
 function Homepage() {
 	usePageTitle('Homepage');
 
 	return (
-		<div>
-			<div className='grid grid-cols-4'>
-				<div className='lg:col-span-1 hidden lg:block'>
-					<WeatherCard />
-					<CalendarCard />
-				</div>
-				<div className='lg:col-span-2 col-span-4'>
-					<CreatePost />
+		<>
+			<div className='lg:col-span-2 col-span-4'>
+				<CreatePost />
 
-					<div className='mb-4'>
-						<HomeFeeds />
-					</div>
-				</div>
-				<div className='lg:col-span-1 hidden lg:block'>
-					<FriendsList />
+				<div className='mb-4'>
+					<HomeFeeds />
 				</div>
 			</div>
-		</div>
+			<div className='lg:col-span-1 hidden lg:block'>
+				<FriendsList />
+			</div>
+		</>
 	);
 }
 

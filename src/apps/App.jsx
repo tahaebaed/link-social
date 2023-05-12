11f0c6c826683f96d91589e16a27d72b35fdc92f
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-12 19:45:46 +0300

    sprint-5 - mohammed - fix spotlight navigation issue

--- src/apps/App.jsx
+++ src/apps/App.jsx
@@ -1,11 +1,7 @@
-import { Center, MantineProvider } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
-import { SpotlightProvider } from '@mantine/spotlight';
 import React, { useEffect } from 'react';
-import { BiSearchAlt } from 'react-icons/bi';
-import { MdSearchOff } from 'react-icons/md';
 import { BrowserRouter } from 'react-router-dom';
-import { ACTIONS } from '../constants/spotlight';
 import CommentsModal from '../modals/Comments';
 import { ipInstance } from '../utilities/weatherCard/ipInstance.js';
 import ErrorBoundary from './ErrorBoundary';
@@ -20,25 +16,9 @@ function App() {
 		<MantineProvider withGlobalStyles theme={{ primaryColor: 'teal' }}>
 			<ErrorBoundary>
 				<ModalsProvider modals={{ comments: CommentsModal }}>
-					<SpotlightProvider
-						actions={ACTIONS}
-						searchIcon={<BiSearchAlt className='text-xl' />}
-						searchPlaceholder='Search...'
-						shortcut={['mod + P', 'mod + K', 'mod + F', '/']}
-						limit={10}
-						nothingFoundMessage={
-							<Center h={200}>
-								<div className='text-center'>
-									<MdSearchOff className='text-4xl' />
-									<p className='m-0'>Nothing found...</p>
-								</div>
-							</Center>
-						}
-					>
-						<BrowserRouter>
-							<MainRoutes />
-						</BrowserRouter>
-					</SpotlightProvider>
+					<BrowserRouter>
+						<MainRoutes />
+					</BrowserRouter>
 				</ModalsProvider>
 			</ErrorBoundary>
 		</MantineProvider>
--- src/layout/navbar/QuerySearch.jsx
+++ src/layout/navbar/QuerySearch.jsx
@@ -1,30 +1,106 @@
-import { Input, Kbd } from '@mantine/core';
-import { spotlight } from '@mantine/spotlight';
+import { Center, Input, Kbd } from '@mantine/core';
+import { SpotlightProvider, spotlight } from '@mantine/spotlight';
 import React from 'react';
 import { BiSearchAlt } from 'react-icons/bi';
+import { MdSearchOff } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import {
+	AiOutlineDashboard,
+	AiOutlineEdit,
+	AiOutlineHome,
+} from 'react-icons/ai';
+import { BsFiles } from 'react-icons/bs';
 
 function QuerySearch() {
+	const navigateTo = useNavigate();
+
+	const ACTIONS = [
+		{
+			title: 'Home',
+			description: 'Get to home page',
+			onTrigger: () => navigateTo('/'),
+			keywords: [
+				'home',
+				'homepage',
+				'mantine',
+				'timeline',
+				'feeds',
+				'create post',
+				'create-post',
+			],
+			group: 'public',
+			icon: <AiOutlineHome size='1.2rem' />,
+		},
+		{
+			title: 'Editor',
+			description: 'Get help on post editor',
+			onTrigger: () => console.log('Editor'),
+			keywords: [
+				'editor',
+				'post',
+				'help',
+				'feed',
+				'create post',
+				'create-post',
+			],
+			group: 'public',
+			icon: <AiOutlineEdit size='1.2rem' />,
+		},
+		{
+			title: 'Dashboard',
+			description: 'Get full information about current system status',
+			onTrigger: () => console.log('Dashboard'),
+			keywords: [],
+			group: 'dashboard',
+			icon: <AiOutlineDashboard size='1.2rem' />,
+		},
+		{
+			title: 'Documentation',
+			description: 'Visit documentation to lean more about all features',
+			onTrigger: () => console.log('Documentation'),
+			keywords: [],
+			group: 'auth',
+			icon: <BsFiles size='1.2rem' />,
+		},
+	];
+
 	return (
-		<Input
-			className='flex-grow'
-			icon={<BiSearchAlt />}
-			radius='xl'
-			onClick={() => spotlight.open()}
-			component='button'
-			styles={(theme) => ({
-				input: {
-					cursor: 'pointer',
-					'&:focus-within': {
-						borderColor: theme.colors.gray[4],
-					},
-				},
-			})}
+		<SpotlightProvider
+			actions={ACTIONS}
+			searchIcon={<BiSearchAlt className='text-xl' />}
+			searchPlaceholder='Search...'
+			shortcut={['mod + P', 'mod + K', 'mod + F', '/']}
+			limit={10}
+			nothingFoundMessage={
+				<Center h={200}>
+					<div className='text-center'>
+						<MdSearchOff className='text-4xl' />
+						<p className='m-0'>Nothing found...</p>
+					</div>
+				</Center>
+			}
 		>
-			<div className='text-gray-400 flex justify-between items-center'>
-				<>Search</>
-				<Kbd className='text-gray-300 text-xs'>/</Kbd>
-			</div>
-		</Input>
+			<Input
+				className='flex-grow'
+				icon={<BiSearchAlt />}
+				radius='xl'
+				onClick={() => spotlight.open()}
+				component='button'
+				styles={(theme) => ({
+					input: {
+						cursor: 'pointer',
+						'&:focus-within': {
+							borderColor: theme.colors.gray[4],
+						},
+					},
+				})}
+			>
+				<div className='text-gray-400 flex justify-between items-center'>
+					<>Search</>
+					<Kbd className='text-gray-300 text-xs'>/</Kbd>
+				</div>
+			</Input>
+		</SpotlightProvider>
 	);
 }
 

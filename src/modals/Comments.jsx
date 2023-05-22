cfc7aa66a966f519fa68c6ea21e99be7adbed2c3
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-22 10:00:31 +0300

    sprint-5 - mohammed - add create-comment functionality

--- src/modals/Comments.jsx
+++ src/modals/Comments.jsx
@@ -7,18 +7,20 @@ import {
 	Textarea,
 	Timeline,
 } from '@mantine/core';
-import { modals } from '@mantine/modals';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { IoAlertCircleOutline } from 'react-icons/io5';
 import { VscCommentDiscussion } from 'react-icons/vsc';
+import { MdErrorOutline } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '../components/Button';
-import { getComment } from '../utilities/store/comments.slice';
+import { createComment, getComment } from '../utilities/store/comments.slice';
+import { updatePostCommentNumber } from '../utilities/store/posts_reducer/postsSlice';
 
 function CommentsModal(props) {
 	const dispatch = useDispatch();
+	const viewport = useRef(null);
 	const { comments, isLoading, error } = useSelector(
-		(store) => store['comments']
+		(store) => store['comments']['retrieve']
 	);
 
 	useEffect(() => {
@@ -46,7 +48,7 @@ function CommentsModal(props) {
 		if (comments.length) {
 			return (
 				<>
-					<ScrollArea h={350} offsetScrollbars>
+					<ScrollArea h={350} offsetScrollbars viewportRef={viewport}>
 						<Timeline
 							color='teal'
 							active={-1}
@@ -72,7 +74,7 @@ function CommentsModal(props) {
 							))}
 						</Timeline>
 					</ScrollArea>
-					<WriteComment />
+					<WriteComment postId={props.postId} viewport={viewport} />
 				</>
 			);
 		}
@@ -85,7 +87,7 @@ function CommentsModal(props) {
 						<p className='text-sm mt-3'>No Comments Found</p>
 					</div>
 				</div>
-				<WriteComment />
+				<WriteComment postId={props.postId} viewport={viewport} />
 			</>
 		);
 	} else {
@@ -93,14 +95,41 @@ function CommentsModal(props) {
 	}
 }
 
-const WriteComment = () => {
+const WriteComment = (props) => {
+	const dispatch = useDispatch();
+	const {
+		isLoading,
+		error,
+		comment: createdComment,
+	} = useSelector((store) => store['comments']['create']);
+	const { comments } = useSelector((store) => store['comments']['retrieve']);
 	const [comment, setComment] = useState('');
 
 	const onFormSubmit = (evt) => {
 		evt.preventDefault();
 
-		console.log(comment);
+		dispatch(
+			createComment({
+				body: comment,
+				postId: props.postId,
+			})
+		);
 	};
+
+	useEffect(() => {
+		setComment('');
+		props.viewport?.current?.scrollTo({
+			top: props.viewport.current.scrollHeight,
+			behavior: 'smooth',
+		});
+
+		dispatch(
+			updatePostCommentNumber({
+				postId: props.postId,
+				comments: comments.length,
+			})
+		);
+	}, [createdComment]);
 
 	return (
 		<form onSubmit={onFormSubmit}>
@@ -110,11 +139,21 @@ const WriteComment = () => {
 				minRows={2}
 				maxRows={5}
 				value={comment}
+				data-autofocus
 				onChange={(evt) => setComment(evt.target.value)}
 			/>
-			<Button type='submit' className='mt-2' disabled={!comment}>
-				Publish
-			</Button>
+			{error && (
+				<Alert icon={<MdErrorOutline />} title='Error!' color='red'>
+					{JSON.stringify(error)}
+				</Alert>
+			)}
+			{isLoading ? (
+				<Loader color='teal' className='mt-2' />
+			) : (
+				<Button type='submit' className='mt-2' disabled={!comment}>
+					Publish
+				</Button>
+			)}
 		</form>
 	);
 };

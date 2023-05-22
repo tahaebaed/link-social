35ebc101adb2c95708e030304bd277af0940fd81
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-22 10:01:00 +0300

    sprint-5 - mohammed - add update-post-comment-number reduxe

--- src/utilities/store/posts_reducer/postsSlice.js
+++ src/utilities/store/posts_reducer/postsSlice.js
@@ -1,30 +1,52 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { userInterceptor } from '../../../apps/axiosInstance';
-import { createPostExtra, getReactExtra, postReactExtra, sharePostExtra } from './postReactsExtraReducer';
+import {
+	createPostExtra,
+	getReactExtra,
+	postReactExtra,
+	sharePostExtra,
+} from './postReactsExtraReducer';
 export const getPosts = createAsyncThunk('getPosts', async (num) => {
 	try {
 		const response = await userInterceptor({
 			method: 'GET',
-			url: `/posts?page=${num}`
-		})
-		console.log(response.data.data.posts, "posts")
+			url: `/posts?page=${num}`,
+		});
+		console.log(response.data.data.posts, 'posts');
 		return response.data.data.posts;
 	} catch (err) {
-		return err.response.data
+		return err.response.data;
 	}
-})
+});
 const initialState = {
-	posts: { loading: false, loadMore: false, currPage: 1, posts: [], error: '', hasMore: true },
+	posts: {
+		loading: false,
+		loadMore: false,
+		currPage: 1,
+		posts: [],
+		error: '',
+		hasMore: true,
+	},
 	reacts: { loading: false, error: '', reacts: [] },
 	createdPost: { loading: false, error: '', post: {} },
 	sharePost: { isLoading: false, error: '', sharedPosts: {} },
-
 };
 
 const postsSlice = createSlice({
 	name: 'posts',
 	initialState,
-	reducers: {},
+	reducers: {
+		updatePostCommentNumber: (state, action) => {
+			state.posts.posts = state.posts.posts.map((post) =>
+				post.id === action.payload.postId
+					? {
+							...post,
+							comments_count: action.payload.comments,
+					  }
+					: post
+			);
+		},
+	},
 	extraReducers: {
 		...postReactExtra(),
 		...getReactExtra(),
@@ -32,30 +54,29 @@ const postsSlice = createSlice({
 		...sharePostExtra(),
 		[getPosts.pending]: (state) => {
 			if (state.posts.currPage === 1) {
-				state.posts.loading = true
+				state.posts.loading = true;
 			} else {
-				state.posts.loadMore = true
+				state.posts.loadMore = true;
 			}
-
 		},
 		[getPosts.fulfilled]: (state, action) => {
 			if (state.posts.currPage === 1) {
-				state.posts.loading = false
+				state.posts.loading = false;
 			} else {
-				state.posts.loadMore = false
+				state.posts.loadMore = false;
 			}
 
-			state.posts.posts = [...state.posts.posts, ...action.payload.data]
-			state.posts.hasMore = action.payload.current_page !== action.payload.last_page
-			state.posts.currPage = action.payload.current_page + 1
-
+			state.posts.posts = [...state.posts.posts, ...action.payload.data];
+			state.posts.hasMore =
+				action.payload.current_page !== action.payload.last_page;
+			state.posts.currPage = action.payload.current_page + 1;
 		},
 		[getPosts.rejected]: (state, action) => {
-			state.posts.loading = false
-			state.posts.error = action.payload.message
-		}
-	}
-})
+			state.posts.loading = false;
+			state.posts.error = action.payload.message;
+		},
+	},
+});
 
-export const { updatePostRect } = postsSlice.actions
-export default postsSlice.reducer
+export const { updatePostRect, updatePostCommentNumber } = postsSlice.actions;
+export default postsSlice.reducer;

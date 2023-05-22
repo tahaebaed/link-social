bbbfbb58705bbb3a1e227bbee366459ce25cdb80
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-22 10:01:21 +0300

    sprint-5 - mohammed - add create-post api endpoint

--- src/utilities/store/comments.slice.js
+++ src/utilities/store/comments.slice.js
@@ -14,34 +14,74 @@ const getComment = createAsyncThunk('get/comment', async (postId) => {
 	}
 });
 
+const createComment = createAsyncThunk(
+	'create/comment',
+	async ({ postId, body }) => {
+		try {
+			const response = await userInterceptor({
+				method: 'post',
+				url: `/comments/${postId}`,
+				data: { body },
+			});
+			return response.data;
+		} catch (err) {
+			return { error: err.response, status: false };
+		}
+	}
+);
+
 const comments = createSlice({
 	name: 'comment',
 	initialState: {
-		isLoading: false,
-		error: null,
-		comments: null,
+		retrieve: { isLoading: false, error: null, comments: null },
+		create: {
+			isLoading: false,
+			error: null,
+			comment: null,
+		},
 	},
 	reducers: {},
 	extraReducers: {
 		[getComment.pending]: (state) => {
-			state.isLoading = true;
+			state.retrieve.isLoading = true;
 		},
 		[getComment.fulfilled]: (state, action) => {
-			state.isLoading = false;
+			state.retrieve.isLoading = false;
 
 			if (action.payload.status) {
-				state.comments = action.payload.data.comments;
+				state.retrieve.comments = action.payload.data.comments;
 			} else {
-				state.error = action.payload.error.data;
+				state.retrieve.error = action.payload.error.data;
 				toast.error(action.payload.error.data.message);
 			}
 		},
 		[getComment.rejected]: (state, action) => {
-			state.error = action.payload.error.data;
+			state.retrieve.error = action.payload.error.data;
+			toast.error(action.payload.error.data.message);
+		},
+		[createComment.pending]: (state) => {
+			state.create.isLoading = true;
+		},
+		[createComment.fulfilled]: (state, action) => {
+			state.create.isLoading = false;
+
+			if (action.payload.status) {
+				state.retrieve.comments = [
+					...state.retrieve.comments,
+					action.payload.data.comment,
+				];
+				state.create.comment = action.payload.data.comment;
+			} else {
+				state.create.error = action.payload.error.data;
+				toast.error(action.payload.error.data.message);
+			}
+		},
+		[createComment.rejected]: (state, action) => {
+			state.create.error = action.payload.error.data;
 			toast.error(action.payload.error.data.message);
 		},
 	},
 });
 
 export default comments.reducer;
-export { getComment };
+export { getComment, createComment };
